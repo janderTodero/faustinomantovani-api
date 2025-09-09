ad861e0ab7fc12cfb26cbd0d26669755f3109655
janderTodero <jandertodero@gmail.com>
2025-09-09 16:28:06 -0300

    password changed

--- generateHash.js
+++ generateHash.js
@@ -2,7 +2,7 @@
 const bcrypt = require('bcrypt');
 
 (async () => {
-  const password = 'jander97';
+  const password = 'Kokobongo123';
   const hash = await bcrypt.hash(password, 10);
   console.log(hash);
 })();
